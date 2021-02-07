6b425d6fa8646bd68ab648154a561fe0d757e185
SafaElmali <tsafaelmali@gmail.com>
2021-02-07 10:23:22 +0300

    refactor: remove unused packages && improve crawler

--- crawler.js
+++ crawler.js
@@ -7,39 +7,20 @@ const checkProductListed = (err, res, done) => {
     // $ is Cheerio by default
     //a lean implementation of core jQuery designed specifically for the server
     const $ = res.$;
+    // Get 'Add To Chart' button text
+    const text = $(".btn-stock").text();
 
-    // Get 'Add To Chart' button's parent div
-    const buttonParentDiv = $(
-      "body > main > div.container-fluid.wrapper-linear > div > div > div > div > div.col-xs-12.col-sm-12.col-md-12.col-lg-6.pd-right > div.container-fluid > div > div > div.d-table.hidden-xs > div.d-cell.product-button--cell"
-    );
-
-    // Get 'Add To Chart' button
-    const productButton = buttonParentDiv[0].childNodes.find(
-      (node) => node.name === "button"
-    );
-
-    /*
-     * Check button has basket class
-     * If it has basket class that means it is on sale
-     * Otherwise, the product not listed for sale still..
-     */
-    const buttonSpanList = productButton.childNodes.filter(
-      (node) => node.name === "span"
-    );
-
-    const textList = buttonSpanList
-      .map((node) => node.childNodes.map((childNode) => childNode.data))
-      .join(",");
-
-    if (textList.includes("SEPETE EKLE")) {
+    if (text.includes("SEPETE EKLE")) {
       console.log("Product on SALE! GO GO GO!");
       player.play("ring.mp3", function (err) {
         if (err) throw err;
       });
-    } else if (textList.includes("ÇOK YAKINDA")) {
+    } else if (text.includes("ÇOK YAKINDA")) {
       console.log("Product not on sale still..");
-    } else if (textList.includes("TÜKENDİ")) {
+    } else if (text.includes("TÜKENDİ")) {
       console.log("You missed it....");
+    }else {
+      console.log("Couldn't read it...");
     }
     console.log("Checking again...");
   }
